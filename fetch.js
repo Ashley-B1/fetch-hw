4e2d9599f5a32a47fd7ae742131389eb60823659
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-05-26 12:32:00 -0500

    added js info and stylesheet

--- fetch.js
+++ fetch.js
@@ -1,1 +1,31 @@
-// console.log('hello from fetch.js');
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(res => res.json())
+  .then(data => applyTodo(data));
+
+const applyTodo = data => {
+  data.forEach(item => {
+    console.log(item);
+    let todoContainer = document.querySelector('#todo-container');
+    let box = document.createElement('div');
+    let boxId = document.createElement('h2');
+    let taskComplete = document.createElement('div');
+    let boxTodo = document.createElement('h3');
+    let checkBox = document.createElement('input');
+    let done = document.createElement('span');
+
+    todoContainer.append(box);
+    box.appendChild(boxId);
+    box.appendChild(taskComplete);
+    taskComplete.appendChild(boxTodo);
+    taskComplete.appendChild(checkBox);
+    taskComplete.appendChild(done);
+
+    box.setAttribute('class', 'todo-info');
+    taskComplete.setAttribute('class', 'complete-section');
+    checkBox.setAttribute('type', 'checkbox');
+
+    boxId.innerText = `${item.id}.`;
+    boxTodo.innerText = item.title;
+    done.innerText = 'Done';
+  });
+};
