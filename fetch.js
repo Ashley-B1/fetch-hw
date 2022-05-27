ccb7db95a81a9db266fd62a8a653729a9397219d
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-05-27 10:55:44 -0500

    added new styling and features

--- fetch.js
+++ fetch.js
@@ -1,31 +1,40 @@
 fetch('https://jsonplaceholder.typicode.com/todos')
   .then(res => res.json())
-  .then(data => applyTodo(data));
+  .then(data => addTodos(data));
 
-const applyTodo = data => {
+const addTodos = data => {
   data.forEach(item => {
     console.log(item);
-    let todoContainer = document.querySelector('#todo-container');
-    let box = document.createElement('div');
-    let boxId = document.createElement('h2');
-    let taskComplete = document.createElement('div');
-    let boxTodo = document.createElement('h3');
-    let checkBox = document.createElement('input');
-    let done = document.createElement('span');
 
-    todoContainer.append(box);
-    box.appendChild(boxId);
-    box.appendChild(taskComplete);
-    taskComplete.appendChild(boxTodo);
-    taskComplete.appendChild(checkBox);
-    taskComplete.appendChild(done);
+    let todoList = document.querySelector('#todo-list');
+    let listTodo = document.createElement('ul');
+    let todoItem = document.createElement('li');
+    let todoLeft = document.createElement('div');
+    let todoRight = document.createElement('div');
 
-    box.setAttribute('class', 'todo-info');
-    taskComplete.setAttribute('class', 'complete-section');
-    checkBox.setAttribute('type', 'checkbox');
-
-    boxId.innerText = `${item.id}.`;
-    boxTodo.innerText = item.title;
-    done.innerText = 'Done';
+    // todoContainer.setAttribute('class', 'todo-list');
   });
 };
+
+// let todoContainer = document.querySelector('#todo-container');
+// let box = document.createElement('div');
+// let boxId = document.createElement('h2');
+// let taskComplete = document.createElement('div');
+// let boxTodo = document.createElement('h3');
+// let checkBox = document.createElement('input');
+// let done = document.createElement('span');
+
+// todoContainer.append(box);
+// box.appendChild(boxId);
+// box.appendChild(taskComplete);
+// taskComplete.appendChild(boxTodo);
+// taskComplete.appendChild(checkBox);
+// taskComplete.appendChild(done);
+
+// box.setAttribute('class', 'todo-info');
+// taskComplete.setAttribute('class', 'complete-section');
+// checkBox.setAttribute('type', 'checkbox');
+
+// boxId.innerText = `${item.id}.`;
+// boxTodo.innerText = item.title;
+// done.innerText = 'Done';
